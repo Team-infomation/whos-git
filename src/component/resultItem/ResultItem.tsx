a5f19be9364353d6d226ad56434d6a99a201e8e3
kkt9102 <kkt9102@gmail.com>
2024-04-19 08:59:53 +0900

    feat : profile add

--- src/component/resultItem/ResultItem.tsx
+++ src/component/resultItem/ResultItem.tsx
@@ -1,16 +1,29 @@
 // MODULE
 import styled from "styled-components";
+// IMAGE
+import Logo from "/logo.png";
 // TYPE
 interface Props {
   login: string;
   id: number;
   type: string;
+  avatar: string;
+  follower: object;
 }
-const ResultItem: React.FC<Props> = ({ login, id, type }) => {
+
+const AvatarBox = styled.div``;
+const ResultItem: React.FC<Props> = ({ login, id, type, avatar, follower }) => {
+  console.log(follower);
   return (
-    <>
+    <div className="flex">
+      <AvatarBox>
+        <img src={avatar} alt="" loading="lazy" width={80} height={80} />
+      </AvatarBox>
       <div>{login}</div>
-    </>
+      <div>{type}</div>
+      <div>Follower : </div>
+      <div>Following : </div>
+    </div>
   );
 };
 
--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -6,18 +6,24 @@ interface Props {}
 import ResultItem from "../../component/resultItem";
 import { searchStore } from "../../store/searchStore";
 const Result: React.FC<Props> = () => {
-  const { searchResult } = searchStore();
+  const { searchResult, keyword } = searchStore();
   const totalCount: number = searchResult.total_count;
-  const resultObject: any = searchResult.items;
+  const resultObject = searchResult.items;
   console.log(searchResult);
   return (
     <>
-      검색결과
+      검색어 {keyword}에 대한 검색결과
       <br />총<span>{totalCount}개</span>의 결과
       <ul>
         {resultObject.map((item: any) => (
           <li key={item.login}>
-            <ResultItem login={item.login} id={item.id} type={item.type} />
+            <ResultItem
+              login={item.login}
+              id={item.id}
+              type={item.type}
+              avatar={item.avatar_url}
+              follower={item.followers_url}
+            />
           </li>
         ))}
       </ul>
