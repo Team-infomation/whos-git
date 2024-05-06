770babe7303e338f18e9a3a04b4e335a40e33886
kkt9102 <kkt9102@gmail.com>
2024-05-06 20:47:33 +0900

    feat : detail file get

--- src/component/repositoryDetail/FileList.tsx
+++ src/component/repositoryDetail/FileList.tsx
@@ -2,6 +2,7 @@
 import styled from "styled-components";
 // API
 import { exloerRepositoryListGET } from "../../api/github";
+import { useLayoutEffect, useState } from "react";
 // STYLED
 const ListBoxFrame = styled.div`
   border: 1px solid var(--light-gray);
@@ -15,6 +16,10 @@ const ListBoxFrame = styled.div`
       }
       &:hover {
         font-weight: 600;
+      }
+      i {
+        margin-right: 1rem;
+        font-size: 1.8rem;
       }
     }
   }
@@ -30,20 +35,25 @@ type ItemMap = {
   type: string;
 };
 const FileList: React.FC<FileListProps> = ({ listData, id, repoName }) => {
+  const [fileResult, setFileResult] = useState<object>([]);
   const getDirectoryFileList = async (name: string) => {
     // console.log(name);
     try {
       const response = await exloerRepositoryListGET(id, repoName, name);
       console.log(response);
+      setFileResult(response);
     } catch (error) {
       console.log(error);
     }
   };
   console.log("listData", listData);
+  useLayoutEffect(() => {
+    setFileResult(listData);
+  }, []);
   return (
     <ListBoxFrame>
       <ul>
-        {listData.map((item: ItemMap) => (
+        {fileResult.map((item: ItemMap) => (
           <li
             onClick={() => getDirectoryFileList(item.name)}
             className="flex flex_jc_s flex_ai_c cursor_p"
